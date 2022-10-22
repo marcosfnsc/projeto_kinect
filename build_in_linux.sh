b1f1269adfd1db4f35613eb260e78676ad66a13c
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-10-21 22:24:05 -0300

    integrado script do vcpkg ao cmake

--- build_in_linux.sh
+++ build_in_linux.sh
@@ -3,3 +3,5 @@
 # vcpkg
 [ ! -d "vcpkg" ] && git clone --depth 1 https://github.com/Microsoft/vcpkg.git
 ./vcpkg/bootstrap-vcpkg.sh -disableMetrics
+
+cmake -B build -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
