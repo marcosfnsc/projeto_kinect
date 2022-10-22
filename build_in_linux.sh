3544273e9091d0d86fa4021abe7bb887547c1f4a
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-10-22 20:08:53 -0300

    removido vcpkg para gerenciar manualmente o donwload e compilação de outras depedencias

--- build_in_linux.sh
+++ build_in_linux.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# vcpkg
-[ ! -d "vcpkg" ] && git clone --depth 1 https://github.com/Microsoft/vcpkg.git
-./vcpkg/bootstrap-vcpkg.sh -disableMetrics
-
-cmake -B build -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
+[ ! -d "third_party/OpenNI2"      ] && git clone --depth 1 --branch v2.2.0-debian https://github.com/structureio/OpenNI2.git      third_party/OpenNI2
+[ ! -d "third_party/freeglut"     ] && git clone --depth 1 --branch v3.4.0        https://github.com/FreeGLUTProject/freeglut.git third_party/freeglut
+[ ! -d "third_party/libfreenect"  ] && git clone --depth 1 --branch v0.6.4        https://github.com/OpenKinect/libfreenect.git   third_party/libfreenect
+[ ! -d "third_party/libfreenect2" ] && git clone --depth 1 --branch v0.2.1        https://github.com/OpenKinect/libfreenect2.git  third_party/libfreenect2
