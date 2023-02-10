1cb7d4606ab6532fca48dee5925d4393ef1bd3bd
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-02-09 22:09:12 -0300

    removido includes desativados e reformatado o codigo

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
-#include <libfreenect2/frame_listener.hpp>
 #include <string>
 
+#include <libfreenect2/frame_listener.hpp>
 #include <libfreenect2/frame_listener_impl.h>
 #include <libfreenect2/libfreenect2.hpp>
 #include <libfreenect2/packet_pipeline.h>
-//#include <libfreenect2/logger.h>
-//#include <libfreenect2/registration.h>
 
 int main() {
   libfreenect2::Freenect2 freenect2;
   libfreenect2::Freenect2Device *dev = 0;
   libfreenect2::PacketPipeline *pipeline = 0;
 
-  if(freenect2.enumerateDevices() == 0) {
+  if (freenect2.enumerateDevices() == 0) {
     std::cout << "no device connected!" << std::endl;
     return -1;
   }
@@ -29,7 +27,9 @@ int main() {
   }
 
   // listener
-  libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Color | libfreenect2::Frame::Depth | libfreenect2::Frame::Ir);
+  libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Color |
+                                                libfreenect2::Frame::Depth |
+                                                libfreenect2::Frame::Ir);
   libfreenect2::FrameMap frames;
 
   // start
