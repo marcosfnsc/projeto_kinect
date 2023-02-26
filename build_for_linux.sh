79facbaa01bea0e63158ae469315fbb6b885a9b2
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-02-26 17:33:03 -0300

    corrigido nome da tag git

--- build_for_linux.sh
+++ build_for_linux.sh
@@ -4,7 +4,7 @@
 [ ! -d "third_party/freeglut"     ] && git clone --depth 1 --branch v3.4.0        https://github.com/FreeGLUTProject/freeglut.git third_party/freeglut
 [ ! -d "third_party/libfreenect"  ] && git clone --depth 1 --branch v0.6.4        https://github.com/OpenKinect/libfreenect.git   third_party/libfreenect
 [ ! -d "third_party/libfreenect2" ] && git clone --depth 1 --branch v0.2.1        https://github.com/OpenKinect/libfreenect2.git  third_party/libfreenect2
-[ ! -d "third_party/opencv"       ] && git clone --depth 1 --branch v4.7.0        https://github.com/opencv/opencv.git            third_party/opencv
+[ ! -d "third_party/opencv"       ] && git clone --depth 1 --branch 4.7.0         https://github.com/opencv/opencv.git            third_party/opencv
 
 WORKDIR=$PWD
 
