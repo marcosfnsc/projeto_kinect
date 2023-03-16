dd35204514cc6c17372d30d4fbdc83348ce05ef1
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-03-16 14:35:39 -0300

    desativado clones de repos ainda não necessarios

--- build_for_linux.sh
+++ build_for_linux.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-[ ! -d "third_party/OpenNI2"      ] && git clone --depth 1 --branch v2.2.0-debian https://github.com/structureio/OpenNI2.git      third_party/OpenNI2
-[ ! -d "third_party/freeglut"     ] && git clone --depth 1 --branch v3.4.0        https://github.com/FreeGLUTProject/freeglut.git third_party/freeglut
-[ ! -d "third_party/libfreenect"  ] && git clone --depth 1 --branch v0.6.4        https://github.com/OpenKinect/libfreenect.git   third_party/libfreenect
+#[ ! -d "third_party/OpenNI2"      ] && git clone --depth 1 --branch v2.2.0-debian https://github.com/structureio/OpenNI2.git      third_party/OpenNI2
+#[ ! -d "third_party/freeglut"     ] && git clone --depth 1 --branch v3.4.0        https://github.com/FreeGLUTProject/freeglut.git third_party/freeglut
+#[ ! -d "third_party/libfreenect"  ] && git clone --depth 1 --branch v0.6.4        https://github.com/OpenKinect/libfreenect.git   third_party/libfreenect
 [ ! -d "third_party/libfreenect2" ] && git clone --depth 1 --branch v0.2.1        https://github.com/OpenKinect/libfreenect2.git  third_party/libfreenect2
 [ ! -d "third_party/opencv"       ] && git clone --depth 1 --branch 4.7.0         https://github.com/opencv/opencv.git            third_party/opencv
 
