27271ef44fa79bf6f68e212e7e2d42ef430673a6
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-03-19 21:06:00 -0300

    adicionado openni2 ao processo de compilação e adionado condições pra nao compilar aterfatos que ja estão prontos

--- build_for_linux.sh
+++ build_for_linux.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-./scripts/build_libfreenect2.sh
+[ ! -d "third_party/OpenNI2"      ] && ./scripts/build_openni2.sh
+[ ! -d "third_party/libfreenect2" ] && ./scripts/build_libfreenect2.sh
 
 echo "compiling the project"
 cmake -B build
