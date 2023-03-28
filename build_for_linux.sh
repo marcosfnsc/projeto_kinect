2ef51ed386095000e5802b4632f5e5c95bad6f05
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-03-28 20:44:43 -0300

    corrigido nome da pasta

--- build_for_linux.sh
+++ build_for_linux.sh
@@ -2,7 +2,7 @@
 
 [ ! -d "third_party/OpenNI2"      ] && ./scripts/build_openni2.sh
 [ ! -d "third_party/libfreenect2" ] && ./scripts/build_libfreenect2.sh
-[ ! -d "third_party/oencv"        ] && ./scripts/build_opencv.sh
+[ ! -d "third_party/opencv"        ] && ./scripts/build_opencv.sh
 
 echo "compiling the project"
 cmake -B build
