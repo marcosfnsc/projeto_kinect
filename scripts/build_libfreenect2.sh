7f24e24188d2c93eb4713c090bbc47fdbfffce48
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-03-28 23:43:43 -0300

    adicionado caminho dos headers do openni

--- scripts/build_libfreenect2.sh
+++ scripts/build_libfreenect2.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 OpenNI2_LIBRARY_DIR="$PWD/third_party/OpenNI2"
+OpenNI2_INCLUDE_DIRS="$PWD/third_party/OpenNI2/include"
 
 [ ! -d "third_party/libfreenect2" ] && git clone --depth 1 --branch v0.2.1 https://github.com/OpenKinect/libfreenect2.git third_party/libfreenect2
 
@@ -16,5 +17,6 @@ cmake \
   -DENABLE_TEGRAJPEG=OFF \
   -DBUILD_EXAMPLES=OFF \
   -DOpenNI2_LIBRARY=$OpenNI2_LIBRARY_DIR \
+  -DOpenNI2_INCLUDE_DIRS=$OpenNI2_INCLUDE_DIRS \
   -DCMAKE_INSTALL_PREFIX=$PWD/..
 make
