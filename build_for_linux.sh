e6ef93b32bfa06c334f01b2c3fe43ca6b114b4e2
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-03-28 23:30:46 -0300

    alterado localização do comando pra suprimir algumas mensagens do git

--- build_for_linux.sh
+++ build_for_linux.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+git config --global advice.detachedHead false # disable checkout message
 [ ! -d "third_party/OpenNI2"      ] && ./scripts/build_openni2.sh
 [ ! -d "third_party/libfreenect2" ] && ./scripts/build_libfreenect2.sh
 [ ! -d "third_party/opencv"        ] && ./scripts/build_opencv.sh
--- scripts/build_openni2.sh
+++ scripts/build_openni2.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-git config advice.detachedHead false # disable checkout message
 [ ! -d "third_party/OpenNI2" ] && git clone --depth 1 --branch v2.2.0-debian https://github.com/structureio/OpenNI2.git third_party/OpenNI2
 
 cd third_party/OpenNI2
